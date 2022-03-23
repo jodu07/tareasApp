93f3072e17ecad383e085d160e6bd895c4f554fc
jodu07 <jhonnat.dussan@gmail.com>
2022-03-23 16:17:45 -0500

    commit 1

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+
+  listTareas: Tarea[] = []
+  nombreTarea = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarTarea(){
+    
+    // crear un objeto tarea
+
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+
+
+
+    // agregar el objeto al array
+    this.listTareas.push(tarea);
+
+
+
+    // reset form
+    this.nombreTarea = ''
+  }
+
+  eliminarTarea(index: number): void{
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(index: number, tarea: Tarea): void{
+    this.listTareas[index].estado = !tarea.estado
+
+  }
+
+}
